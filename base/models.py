d05c9f2ff29bc6574e210b9f0db6ec0c49d7c863
its-priya <priyamissu1999@gmail.com>
2021-05-30 23:18:37 +0530

    Model creation

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from froala_editor.fields import FroalaField
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length= 1000)
+    content = FroalaField()
+    slug = models.SlugField(max_length=1000)
+    image = models.ImageField(upload_to='myBlogs')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.urls.conf import include
 from . import views
 
 urlpatterns = [
     path('', views.home, name= 'home'),
     path('login/', views.login, name= 'login'),
     path('register/', views.register, name= 'register'),
-    path('details/', views.details, name='details')
+    path('details/', views.details, name='details'),
 ]
