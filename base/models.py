03a3b5630725158cda6fce100eb200f9becb9b8d
its-priya <priyamissu1999@gmail.com>
2021-05-31 11:24:10 +0530

    Updated Model fields

--- base/models.py
+++ base/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from froala_editor.fields import FroalaField
+from base.utils import generate_slug
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length= 1000)
     content = FroalaField()
-    slug = models.SlugField(max_length=1000)
-    image = models.ImageField(upload_to='myBlogs')
+    slug = models.SlugField(max_length=1000, null= True, blank= True)
+    image = models.ImageField(upload_to='blog', blank= True, null= True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
     
+    def save(self, *args, **kwargs):
+        self.slug = generate_slug(self.title)
+        super(Blog, self).save(*args, **kwargs)
